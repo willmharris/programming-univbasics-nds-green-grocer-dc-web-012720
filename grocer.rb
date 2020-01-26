71f9ef187bdd7885bca07c2642f0f5320c1c9f7d
William Harris <willmharris0@gmail.com>
2020-01-26 03:43:05 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -13,7 +13,7 @@ def consolidate_cart(cart)
   new_cart = [] 
   counter = 0 
   while counter < cart.length 
-    new_cart_item = find_item_by_name_in_collection(cart[counter][:item], )
+    new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart) 
 end
 
 def apply_coupons(cart, coupons)
