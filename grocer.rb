36edd0728d6009bb1923776135e56f4dd14b1d2e
William Harris <willmharris0@gmail.com>
2020-01-26 03:43:39 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -15,7 +15,8 @@ def consolidate_cart(cart)
   while counter < cart.length 
     new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart) 
     if new_cart_item =! nil 
-      new_cart_item 
+      new_cart_item[:count] += 1 
+      
 end
 
 def apply_coupons(cart, coupons)
