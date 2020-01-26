84dd53cfc77d3caa4097bb73f81d7cb8016ca5d7
William Harris <willmharris0@gmail.com>
2020-01-26 03:42:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -10,28 +10,7 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
-  consolidated_array = []
-  counter2 = 0 
-  while counter2 < cart.length do 
-    name = cart[counter2][:item] 
-    if find_item_by_name_in_collection(name, consolidated_array) == nil
-      consolidated_array.push(cart[counter2]) 
-      consolidated_array[-1][:count] = 1  
-    else 
-      item_location = 0 
-      counter3 = 0 
-      while counter3 < consolidated_array.size do 
-        tempname = consolidated_array[counter3][:item]
-        if tempname  == name 
-          return item_location = counter3 
-        end 
-        counter3 += 1 
-      end 
-      consolidated_array[item_location][:count] += 1 
-    end 
-    counter2 += 1 
-  end 
-  consolidated_array 
+  
 end
 
 def apply_coupons(cart, coupons)
